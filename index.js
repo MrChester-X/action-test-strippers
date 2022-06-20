1a69311627b2476daa4203da45223a4b1494c80f
MrChester-X <bulychev_platon1@mail.ru>
2022-06-20 14:00:53 +0300

    Add test stripper

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 
-let files = fs.readdirSync("./");
+let files = fs.readdirSync("addons/stripper/maps");
 
 for (let file of files) {
     core.warning(`${file}`)
