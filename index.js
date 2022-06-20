1d5ae8f724749c3f1b9834463df548ce9e209d13
MrChester-X <bulychev_platon1@mail.ru>
2022-06-20 12:54:21 +0300

    Fix folder in NodeJS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 
-let files = fs.readdirSync("");
+let files = fs.readdirSync("./");
 
 for (let file of files) {
     core.warning(`${file}`)
